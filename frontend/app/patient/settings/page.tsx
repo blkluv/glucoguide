4a0a9a51f374422ea7d42246734ec5d0ab4b773e
F I R E <firethedev@gmail.com>
2025-01-25 19:37:23 +0600

    settings page typo fixed

--- frontend/app/patient/settings/page.tsx
+++ frontend/app/patient/settings/page.tsx
@@ -4,7 +4,7 @@ export default function SettingsPage() {
   return (
     <div className="px-4 lg:px-8">
       <div className="mt-4 center flex-col md:items-start">
-        <h1 className="text-xl font-semibold leading-6">Apperance</h1>
+        <h1 className="text-xl font-semibold leading-6">Appearance</h1>
         <p className="text-sm">Select or customize your UI theme.</p>
       </div>
       <ThemeUI />
